8627e6a9e0e672f9a758f1e7ef81641e757c4ce1
Hadi Rahimi <59468706+rahimih@users.noreply.github.com>
2025-11-02 19:32:10 -0500

    Add files via upload

---
+++ Script/ingest_data.py
@@ -0,0 +1,29 @@
+import psycopg2
+import random
+from datetime import datetime
+from pip import load_dotenv
+import os
+
+load_dotenv()
+conn = psycopg2.connect(
+    host=os.getenv("localhost"),
+    dbname=os.getenv("iot_data"),
+    dbschema=os.getenv("iot_data_s");
+    user=os.getenv("postgres"),
+    password=os.getenv("Admin980*"),
+    port=os.getenv("5432")
+)
+cur = conn.cursor()
+
+for _ in range(1000):
+    device_id = random.randint(1, 5)
+    temp = random.uniform(20.0, 30.0)
+    humidity = random.uniform(40.0, 60.0)
+    timestamp = datetime.now()
+    cur.execute(
+        "INSERT INTO sensor_readings (device_id, timestamp, temperature, humidity) VALUES (%s, %s, %s, %s)",
+        (device_id, timestamp, temp, humidity)
+    )
+conn.commit()
+cur.close()
+conn.close()
